04b6141eb074553977784e03c1b47958f218c83d
Willian Carvalho <willian.carvalho@instana.com>
2021-01-07 11:33:26 +0100

    test: using default AWS env variable names

--- packages/collector/test/tracing/cloud/aws/sqs/temp.js
+++ packages/collector/test/tracing/cloud/aws/sqs/temp.js
@@ -1,35 +1,8 @@
-/**
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
-
-// ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
-// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sqs-examples-managing-visibility-timeout.html
-
-// Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
 
-const credentails = new AWS.Credentials(process.env.AWS_ACCESS_KEY, process.env.AWS_SECRET_ACCESS);
-// Set the region to us-west-2
-AWS.config.update({
-  region: 'us-east-2',
-  credentials: credentails
-});
+AWS.config.update({region: 'us-east-2'});
 
-// Create the SQS service object
-const sqs = new AWS.SQS({
-  apiVersion: '2012-11-05',
-});
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
 const queueURL = 'https://sqs.us-east-2.amazonaws.com/410797082306/willian-queue';
 
@@ -40,26 +13,28 @@ const params = {
   QueueUrl: queueURL
 };
 
-sqs.receiveMessage(params, function (err, data) {
+sqs.receiveMessage(params, (err, data) => {
   if (err) {
-    console.log('Receive Error', err)
+    console.log('Receive Error', err);
   } else {
     // Make sure we have a message
-    if (data.Messages != null) {
+    if (data.Messages !== null && data.Messages.length > 0) {
+      console.log('data:', data);
       const visibilityParams = {
         QueueUrl: queueURL,
         ReceiptHandle: data.Messages[0].ReceiptHandle,
-        VisibilityTimeout: 20 // 20 second timeout
+        VisibilityTimeout: 20
       }
-      sqs.changeMessageVisibility(visibilityParams, function (err, data) {
+
+      sqs.changeMessageVisibility(visibilityParams, (err, data) => {
         if (err) {
-          console.log('Delete Error', err)
+          console.log('Delete Error', err);
         } else {
-          console.log('Timeout Changed', data)
+          console.log('Timeout Changed', data);
         }
-      })
+      });
     } else {
-      console.log('No messages to change')
+      console.log('No messages to change');
     }
   }
 });
